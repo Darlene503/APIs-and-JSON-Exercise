0ed7ff8c7e25809e16dd8db1fd8110a0e1452dc8
Darlene503 <duyn503@gmail.com>
2023-08-22 21:22:04 -0700

    kanyevsRon

--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -2,11 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public static class RonVSKanyeAPI
     {
+        public static void Convo()
+        {
+            var client = new HttpClient();  
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye says, '{KanyeSaid(client)}'\n");
+                Thread.Sleep(2000);
+                Console.WriteLine($"Ron says, '{RonSaid(client)}'\n '");
+                Thread.Sleep(2000);
+            }
+        }
+
+        private static string KanyeSaid(HttpClient client)
+        {
+            var jtext = client.GetStringAsync("https://api.kanye.rest/").Result;
+            var quote = JObject.Parse(jtext).GetValue("quote").ToString();
+            return quote;
+        }
+
+        private static string RonSaid(HttpClient client)
+        {
+            var jtext = client.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes").Result;
+            var qoute = JArray.Parse(jtext).ToString().Replace('[', ' ').Replace(']', ' ').Replace('"',' ').Trim();
+            return qoute; 
+        }
     }
 }
